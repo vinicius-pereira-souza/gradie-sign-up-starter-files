c1544e38d21d0d32e95a653d1ac8b471b078ed58
Vinicius Pereira de Souza <vinicius.pereira.souza98@gmail.com>
2023-06-22 14:08:42 -0300

    template para telas de mobile completo

---
+++ tailwind.config.js
@@ -0,0 +1,29 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        "dark-blue": "#013243",
+        "light-blue": "#82BBCF",
+        "light-gray": "#A7A7A7",
+      },
+      backgroundImage: {
+        body: 'url("../images/Background.png")',
+      },
+      fontFamily: {
+        Roboto: ["Roboto", "sans-serif"],
+      },
+      fontSize: {
+        33: "33px",
+        34: "34px",
+      },
+      maxWidth: {
+        445: "445px",
+        860: "860px",
+        70: "70%",
+      },
+    },
+  },
+  plugins: [],
+};
